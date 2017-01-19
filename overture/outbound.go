8471614d9a13e1444301b68b52cb80881c8f36f0
sh1r0 <sh1r0@users.noreply.github.com>
2017-01-19 09:10:53 +0800

    Check error of dns response

--- overture/outbound.go
+++ overture/outbound.go
@@ -2,9 +2,10 @@ package overture
 
 import (
 	"errors"
-	"github.com/miekg/dns"
 	"reflect"
 	"time"
+
+	"github.com/miekg/dns"
 )
 
 func getResponse(response_message *dns.Msg, question_message *dns.Msg, inbound_ip string, dns_server dnsServer) error {
@@ -17,7 +18,9 @@ func getResponse(response_message *dns.Msg, question_message *dns.Msg, inbound_i
 	client.Net = dns_server.Protocol
 	client.Timeout = time.Duration(Config.Timeout) * time.Second
 	temp_message, _, err := client.Exchange(question_message, dns_server.Address)
-	if temp_message == nil {
+	if err != nil {
+		return err
+	} else if temp_message == nil {
 		err = errors.New("Response message is nil, maybe timeout")
 		return err
 	}
